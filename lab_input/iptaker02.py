fd579d4b2790b90ea0a36a8ae54ca48f9280b83d
Weijun124 <weijunhuang5@gmail.com>
2022-09-28 19:30:04 +0000

    learning about input function

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   print() - concatenate vs print a series of items"""
+
+def main():
+
+    user_input = input("Please enter an IPv4 IP address:")
+    print("You told me the IPv4 address is:", user_input)
+
+    print("You vendor name is",input("Please input the vendor name: "))
+    
+main()
+
