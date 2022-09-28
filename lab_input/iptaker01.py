a8a2931018324885d172a1c6f0fc3e68f9f5786a
Weijun124 <weijunhuang5@gmail.com>
2022-09-28 19:42:37 +0000

    learning about input function

--- lab_input/iptaker01.py
+++ lab_input/iptaker01.py
@@ -5,4 +5,5 @@
 def main(): 
     user_input=input("Please enter an IPv4 IP address: ")
     print("You told me the IPv4 address is: "+user_input)
+
 main()
--- lab_input/iptaker02.py
+++ lab_input/iptaker02.py
@@ -7,7 +7,6 @@ def main():
     user_input = input("Please enter an IPv4 IP address:")
     print("You told me the IPv4 address is:", user_input)
 
-    print("You vendor name is",input("Please input the vendor name: "))
-    
+    print("You vendor name is",input("Please input the vendor name: "))    
 main()
 
