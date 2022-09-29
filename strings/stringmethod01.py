b50fccafb7d355bc7bf1aed6053b80378199550b
Weijun124 <weijunhuang5@gmail.com>
2022-09-29 17:41:08 +0000

    join and split

---
+++ strings/stringmethod01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""aLTA3 RESEARCH || Author: RZFeeser@alta3.com"""
+
+def main():
+    """ Run-time code """
+    string1="Alta3 Research offers classes on Python coding"
+    newlist=string1.split(" ")
+    print(newlist)
+
+    myiplist=["192","168","0","12"]
+    singleip=".".join(myiplist)
+    print(singleip)
+
+main()
