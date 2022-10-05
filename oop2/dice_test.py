54afe41410a34ec4f7f9935ba7efbfcc254dbbb9
Weijun124 <weijunhuang5@gmail.com>
2022-10-05 15:46:52 +0000

    utilizing classes

---
+++ oop2/dice_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+# imports from cheadice.py (this is in the local directory)
+from cheatdice import Player
+from cheatdice import Cheat_Swapper
+from cheatdice import Cheat_Loaded_Dice
+
+def main():
+    cheater1=Cheat_Swapper() # ability is to change 3rd dice roll to 6
+    cheater2= Cheat_Loaded_Dice()
+
+    cheater1.roll()
+    cheater2.roll()
+    
+    cheater1.cheat()
+    cheater2.cheat()
+
+    print(f"Cheater 1 rolled {cheater1.get_dice()}")
+    print(f"Cheater 2 rolled {cheater2.get_dice()}")
+
+    if sum(cheater1.get_dice()) == sum(cheater2.get_dice()):
+        print("Draw!")
+
+    elif sum(cheater1.get_dice()) > sum(cheater2.get_dice()):
+        print("Cheater 1 wins!")
+
+    else:
+        print("Cheater 2 wins!")
+
+if __name__ == "__main__":
+    main()
