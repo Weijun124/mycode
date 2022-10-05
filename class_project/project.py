b01a179c81aff96a5cee7c075cff9d07456168a8
Weijun124 <weijunhuang5@gmail.com>
2022-10-05 13:12:47 +0000

    draft

--- class_project/project.py
+++ class_project/project.py
@@ -1,37 +1,40 @@
 #!/usr/bin/env python3
 import pandas as pd
+""" Alta3 Project | Weijun Huang
+A script to convert a CSV file to a excel file by using pandas.
+The "Toyota used car listing" was used for user to customize the information they want
+and create their own excel based on this CSV file
+Data website: https://www.kaggle.com/datasets/mysarahmadbhat/toyota-used-car-listing"""
 
 # Load the csv file and sorted by model' alphabet a=>z and create a python dictionary 
 CSV=pd.read_csv("toyota_data.csv")
 CSVSORT=CSV.sort_values('model')
 DATAS=CSVSORT.to_dict(orient='records')
-
+NEWLIST=[]
 
 # Function for filter the model data
 def model_choose(model_opt,model_list):
-    new_list=[]
     if model_opt.lower()!='y':
         model_input=input(f"What type of model you are looking for? We have {model_list}\n ")
-        for data in DATAS:
-            if data['model'].strip().lower()==model_input.strip().lower():
-                new_list.append(data)
-        new_list=price_choose(new_list)
-        print(new_list)
+        for model in DATAS:
+            if model['model'].strip().lower()==model_input.strip().lower():
+                NEWLIST.append(model)
+        price_choose()
+        NEWLIST.to_excel("test1.xls", index=False)
     else:
         print(CSVSORT)
 
 
 # Function for filter the price level
-def price_choose(new_list):
+def price_choose():
     min_price=input("Enter the least price you want\n")
     max_price=input("Enter the highest price you want\n")
     # Use list.copy() to avoid locate bug if you try to remove element from origional list
-    for ele in new_list.copy():
+    for ele in NEWLIST.copy():
         if ele['price']<int(min_price) or ele['price']>int(max_price):
-            new_list.remove(ele)
+            NEWLIST.remove(ele)
         else:
             pass
-    return new_list
 
 
 
