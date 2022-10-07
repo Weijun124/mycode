67b84314102fd30a47fec7610ef703f8d7548d07
Weijun124 <weijunhuang5@gmail.com>
2022-10-07 01:25:00 +0000

    final

--- class_project/function.py
+++ class_project/function.py
@@ -1,51 +1,51 @@
-import re
+""" Alta3 Project | Weijun Huang
+    This is helping function file
+    to execute the research and coverte file to xlsx """
+
 import pandas as pd
 import fontstyle as ft
+import pyfiglet as pf
 
 def model_choose(csvsort):
     """Function for filter the model data"""
     model_list=select_set(csvsort['model'])
     model_input=input(f" What type of model you are looking for?\nWe have {model_list}\n>")
-    print(ft.apply("\nCreating the table...\n", 'green_bg'))
-    check_input(model_list,'model',model_input,csvsort)
+    print(ft.apply("\nCreating the table...", 'green_bg'))
+    check_input(model_list,'model',model_input,csvsort,model_choose)
 
 def price_choose(csvsort):
     """Function for filter the price level"""
-    min_price=input(" Enter the least price you want?\
-        (Lowest price in your currentmodel is ${:,})\n>".format(csvsort['price'].min()))
-    max_price=input(" Enter the highest price you want?\
-        (Highest price in your current model is ${:,})\n>".format(csvsort['price'].max()))
-    price_list=[min_price,max_price]
+    sentence=f" (Your current model price range is\n\
+ between ${csvsort['price'].min():,} and ${csvsort['price'].max():,})"
+    min_price=input(f" Enter the least price you want? {sentence}\n>")
+    max_price=input(f" Enter the highest price you want? {sentence}\n>")
     try:
-        # Using regex to verify valid input
-        for i in price_list:
-            re.match(r'\d+',i)
+        price_list=[int(min_price),int(max_price)]
+        price_list.sort()
         # Use df.copy() to avoid locate bug if you try to remove element from origional list
-        if int(min_price)>=csvsort['price'].min() and int(max_price)<=csvsort['price'].max():
+        if price_list[0]>=csvsort['price'].min() and price_list[1]<=csvsort['price'].max():
             for index, price in csvsort.copy(deep=False).iterrows():
-                if price['price']<int(min_price) or price['price']>int(max_price):
+                if price['price']<price_list[0] or price['price']>price_list[1]:
                     csvsort.drop(index, inplace=True)
         else:
-            print(ft.apply("\n Price is out of the range. Please enter\
-            the range between lowest and highest price we provided \n>"))
+            print(ft.apply("\n Please enter "\
+                 "the price within the range we provided \n"))
             price_choose(csvsort)
-    except:
-        print(ft.apply("\n Invalid input, Please try again\n", 'red-bg'))
+    except ValueError:
+        print(ft.apply("\n Invalid input, Please try again", 'red_bg'))
         price_choose(csvsort)
 
 def transmission_type(csvsort):
     """Function for filter the transmission"""
     transmission_list=select_set(csvsort["transmission"])
-    type_input=input(f" Based on the model and price you chose,\
-we have {transmission_list} for you to choose\n>")
-    check_input(transmission_list,"transmission",type_input,csvsort)
+    type_input=input(f" Based on your selection, we have {transmission_list} for you to choose\n>")
+    check_input(transmission_list,"transmission",type_input,csvsort,transmission_type)
 
 def fuel_type_choose(csvsort):
     """Function for filter the fuel type"""
     fuel_type_list=select_set(csvsort['fuelType'])
-    type_input=input(f" Based on the model, price and transmission\
-you chose, we have {fuel_type_list} for you to choose\n>")
-    check_input(fuel_type_list,'fuelType',type_input,csvsort)
+    type_input=input(f" Based on your selection, we have {fuel_type_list} for you to choose\n>")
+    check_input(fuel_type_list,'fuelType',type_input,csvsort,fuel_type_choose)
 
 def select_set(model):
     """Function to create the selection list for model type and fuel type"""
@@ -57,19 +57,7 @@ def select_set(model):
             pass
     return type_list
 
-def convert_to_excel(csvsort):
-    """Function to show the data or convert it to excel with xlsx form"""
-    convert=input(" Do you want to convert it to a excel file? [Y/n]\n>")
-    if convert.lower()=='y':
-        file_name=input("Please enter the name for the file\n>")
-            
-            # pd.DataFrame(csvsort).to_excel(f"{file_name}.xlsx", index=False)
-            print(ft.apply("\n Invalid input, Please try again\n", 'red-bg'))
-            convert_to_excel(csvsort)
-    else:
-        print(csvsort)
-
-def check_input(user_list,search_type,user_input,csvsort):
+def check_input(user_list,search_type,user_input,csvsort,function_name):
     """Function to check the user input"""
     user_search=False
     # Verify the user input is valid or not
@@ -82,5 +70,26 @@ def check_input(user_list,search_type,user_input,csvsort):
             if data[search_type].strip().lower()!=user_input.strip().lower():
                 csvsort.drop(index, inplace=True)
     else:
-        print(ft.apply("\n Invalid input. Please try again\n", 'red_bg'))
-        model_choose(csvsort)
+        print(ft.apply("\n Invalid input. Please try again", 'red_bg'))
+        function_name(csvsort)
+
+def convert_to_excel(csvsort):
+    """Function to show the data or convert it to excel with xlsx form"""
+    convert=input(" Do you want to convert it to a excel file? [Y/n]\n>")
+    if convert.lower()=='y':
+        file_name=input("Please enter the name for the file\n>")
+        try:
+            print(ft.apply("\nCreating the excel...", 'green_bg'))
+            pd.DataFrame(csvsort).to_excel(f"{file_name}.xlsx", index=False)
+            end="File Is Created! Thanks For Using"
+            print(ft.apply(pf.figlet_format(end, font = "digital" ), 'yellow'))
+        except ValueError:
+            print(ft.apply("\n Invalid input, Please try again\n", 'red-bg'))
+            convert_to_excel(csvsort)
+    elif convert.lower()=='n':
+        print(csvsort)
+        print(f' Total results are {len(csvsort.index)}')
+        end="Thanks For Using"
+        print(ft.apply(pf.figlet_format(end, font = "digital" ), 'yellow'))
+    else:
+        convert_to_excel(csvsort)
--- class_project/project.py
+++ class_project/project.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Alta3 Project | Weijun Huang
-A script to query the information from CSV data for user
-and convert it to a excel file by using pandas.
-The "Toyota used car listing" was used for user to customize the information they want
-and create their own excel based on this CSV file
-Data website: https://www.kaggle.com/datasets/mysarahmadbhat/toyota-used-car-listing"""
+    A script to query the information from CSV data for user
+    and convert it to a excel file by using pandas.
+    The "Toyota used car listing" was used for user to customize the information they want
+    and create their own excel based on this CSV file
+    Data website: https://www.kaggle.com/datasets/mysarahmadbhat/toyota-used-car-listing """
 
 import pandas as pd
 import fontstyle as ft
@@ -15,24 +15,41 @@ from function import transmission_type
 from function import fuel_type_choose
 from function import convert_to_excel
 
-def main():
-    """Load the csv file and sorted by model's alphabet a=>z
-    user can choose to check all the data or
-    do the detail search by putting condition """
-    data=pd.read_csv("toyota_data.csv")
-    csvsort=data.sort_values('model',ignore_index=True)
-    start="' Toyota used car listing ' for you to use"
-    print(ft.apply(pf.figlet_format(start, font = "slant" ), 'yellow'))
-    choose=" Press 'y' for checkout all the data in the list, Press other for specific search\n>"
+def start_run(csvsort):
+    """Main Application Workflow"""
+    print(ft.apply(pf.figlet_format("' Toyota used car listing ' "\
+        "for you to use", font = "slant" ), 'yellow'))
+    choose=" Press 'y' for checkout all the data in"\
+         " the list. Press 'q' to retype the file name. Press other for specific search\n>"
     user_opt=input(choose)
-    if user_opt.lower()!='y':
+    if user_opt.lower()=='y':
+        convert_to_excel(csvsort)
+    elif user_opt.lower()=='q':
+        main()
+    else:
         model_choose(csvsort)
         price_choose(csvsort)
         transmission_type(csvsort)
         fuel_type_choose(csvsort)
         convert_to_excel(csvsort)
-    else:
-        convert_to_excel(csvsort)
+
+def main():
+    """Load the csv file and sorted by model's alphabet order a=>z
+        user can choose to check all the data or
+        do the detail search by putting condition """
+    try:
+        default = 'toyota_data.csv'
+        file_name = input("Enter the file name you want to search"\
+                    " (default setting 'toyota_data.csv') \n>") or default
+        data=pd.read_csv(file_name)
+        csvsort=data.sort_values('model',ignore_index=True)
+        start_run(csvsort)
+    except FileNotFoundError as error:
+        print(f'{error}, please enter correct file name\n')
+        main()
+    except Exception as error:
+        print(f'{error}, please contact tpatrick@alta3.com\n')
+        main()
 
 if __name__ == "__main__":
     main()
